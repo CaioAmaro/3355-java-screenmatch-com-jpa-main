e2d5d856d31541024323ffe283d9e7ea34ac7d10
Hammarok <caioamarocci@gmail.com>
2024-11-27 09:11:29 -0300

    atualizações

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,21 +1,40 @@
 package br.com.alura.screenmatch.model;
 
+import br.com.alura.screenmatch.service.traducao.ConsultaMyMemory;
+import jakarta.persistence.*;
+
+import java.util.List;
 import java.util.Optional;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
+
     private String atores;
     private String sinopse;
     private String poster;
     private Integer totalTemporadas;
     private Double avaliacao;
 
+    @Transient
+    private List<Episodio> episodios;
+
+    public Serie(){}
+
    public Serie(DadosSerie dadosSerie){
        this.titulo = dadosSerie.titulo();
        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
        this.atores = dadosSerie.atores();
-       this.sinopse = dadosSerie.sinopse();
+       this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse());
        this.poster = dadosSerie.poster();
        this.totalTemporadas = dadosSerie.totalTemporadas();
        this.avaliacao = Optional.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0.0);
